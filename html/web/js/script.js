61c50be654ecd0a4226032d561d960a344ee291d
bjl0615 <bjl0615@gmail.com>
2021-09-09 23:39:00 +0900

    Add JS

--- html/web/js/script.js
+++ html/web/js/script.js
@@ -22,3 +22,16 @@ $(".tit .btn").click(function(e){
     $("#cont_nav").slideToggle(200);
     $(this).toggleClass("on");
 });
+
+//ÅÇ¸Þ´º
+const $tab_list = $(".tab_menu");
+
+$tab_list.find("ul ul").hide();
+$tab_list.find("li.active > ul ").show();
+
+function tabMenu(e){
+    e.preventDefault();
+    const $this = $(this);
+    $this.next("ul").show().parent("li").addClass("active").siblings("li").removeClass("active").find(">ul").hide();
+}
+$tab_list.find("ul>li>a").click(tabMenu).focus(tabMenu);
